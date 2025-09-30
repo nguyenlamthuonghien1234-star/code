f15063bf4e5a6a1107bc931a2033dc9ca7edad23
nguyenlamthuonghien1234-star <nguyenlamthuonghien1234@gmail.com>
2025-09-30 17:36:46 +0700

    small calculator

--- main.cpp
+++ main.cpp
@@ -1,31 +1,31 @@
-#include <algorithm>
-#include <iostream>
 #include <iostream>
 using namespace std;
 
-int main() {
-    int x1, y1, x2, y2;
-    cin >> x1 >> y1;
-    cin >> x2 >> y2;
-
-    int dx = x2 - x1;
-    int dy = y2 - y1;
-    
-    int x3 = x2 + dy, y3 = y2 - dx;
-    int x4 = x1 + dy, y4 = y1 - dx;
-
-    cout << "(" << x1 << "," << y1 << ")"
-         << "(" << x2 << "," << y2 << ")"
-         << "(" << x3 << "," << y3 << ")"
-         << "(" << x4 << "," << y4 << ")\n";
-
-    x3 = x2 - dy; y3 = y2 + dx;
-    x4 = x1 - dy; y4 = y1 + dx;
-
-    cout << "(" << x1 << "," << y1 << ")"
-         << "(" << x2 << "," << y2 << ")"
-         << "(" << x3 << "," << y3 << ")"
-         << "(" << x4 << "," << y4 << ")\n";
-
-    return 0;
+int main () {
+    double num1, num2, ans;
+    char dau;
+    cout << "Please enter two numbers and add +-*/";
+    cin >> num1 >> dau >> num2;
+    switch (dau) {
+        case '+':
+        ans = num1 + num2;
+        cout << ans << endl;
+        break;
+        case '-':
+        ans = num1 - num2;
+        cout << ans << endl;
+        break;
+        case '*':
+        ans = num1 * num2;
+        cout << ans << endl;
+        break;
+        case '/':
+        ans = num1 / num2;
+        cout << ans << endl;
+        break;
+        default:
+            cout << "Please enter a number and add /";
+            break;
 }
+            return 0;
+    }
